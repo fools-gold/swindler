23504edf68b6ddd559b09be515fe6f597f1b1b1e
Woongcheol Yang <woongcheol@gmail.com>
2015-04-08 22:05:40 +0900

    Add User#admin
    Authenticate using devise and manually authorize access to rails_admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -2,14 +2,16 @@ RailsAdmin.config do |config|
 
   ### Popular gems integration
 
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+  ## == Authenticate using Devise ==
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
 
-  ## == Cancan ==
-  # config.authorize_with :cancan
+  ## == Authorize manually ==
+  config.authorize_with do
+    redirect_to main_app.root_path unless current_user.try(:admin?)
+  end
 
   ## == PaperTrail ==
   # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
