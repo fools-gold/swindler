f6313a8fe025c581f936bda9582be380520f0a5d
Hoyoon Jun <zeratul9901@gmail.com>
2015-05-26 19:18:27 +0900

    Add Story#photo

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,10 +3,17 @@ class Story < ActiveRecord::Base
   belongs_to :of, class_name: "User", counter_cache: :stories_of_count
   belongs_to :game, counter_cache: true
 
+  has_attached_file :photo,
+    styles: { original: "1280x1280>", medium: "640x640>", thumb: "100x100>" },
+    default_style: :medium
+
   validates :body, length: { maximum: 140 }, presence: true
   validates :by, presence: true
   validates :of, presence: true
   validates :game, presence: true
+
+  validates_attachment_content_type :photo, content_type: %w(image/jpeg image/gif image/png)
+  validates_attachment_size :photo, less_than: 3.megabytes
 
   validate :must_not_make_story_of_oneself
 
